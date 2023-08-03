dd9bfb6e47ac3786f2ef98e3b405463b8fea5b8c
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-08-03 18:42:20 -0500

    adjusted layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 import { info } from "@/data/data";
 
 export default function Home() {
+  const create_link = (name: string, link: string) => {
+    return (
+      <a className='text-blue-500' href={link} target='_blank' rel='noopener noreferrer'>
+        {name}
+      </a>
+    );
+  };
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-10 lg:p-24 '>
       <div className='w-full max-w-6xl text-white font-mono text-xl uppercase grid gap-5 lg:grid-cols-1'>
@@ -14,33 +22,12 @@ export default function Home() {
       <div className='w-full max-w-6xl text-white font-mono text-xl grid gap-5 lg:grid-cols-2'>
         <div>How to Get involved:</div>
         <ul>
+          <li>Follow us on {create_link("Instagram", info.instagram_link)}</li>
           <li>
-            Follow us on{" "}
-            <a className='text-blue-500' href={info.instagram_link}>
-              Instagram
-            </a>{" "}
-            and{" "}
-            <a className='text-blue-500' href={info.twitter_link}>
-              Twitter
-            </a>
+            Attend our {create_link("Ethereum", info.ethereum_meetup_link)} and{" "}
+            {create_link("Web3", info.web3_meetup_link)} Meetups
           </li>
-          <li>
-            Attend our{" "}
-            <a className='text-blue-500' href={info.ethereum_meetup_link}>
-              Ethereum
-            </a>{" "}
-            and{" "}
-            <a className='text-blue-500' href={info.web3_meetup_link}>
-              Web3
-            </a>{" "}
-            Meetups
-          </li>
-          <li>
-            Contribute to our open-source project on{" "}
-            <a className='text-blue-500' href={info.github_link}>
-              Github
-            </a>{" "}
-          </li>
+          <li>Contribute to our open-source project on {create_link("Github", info.github_link)} </li>
         </ul>
       </div>
     </main>

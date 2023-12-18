e3ff3f0a340d25fc9e546d44d464d8d1005cd5f6
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-12-18 12:32:57 -0600

    added click action and menu behavior

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,14 +13,6 @@ interface MenuItemProps {
   link: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ tag, link }) => {
-  return (
-    <div className='menu-item'>
-      <Link href={link}>{tag}</Link>
-    </div>
-  );
-};
-
 const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
@@ -28,39 +20,46 @@ const Header: React.FC = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const MenuItem: React.FC<MenuItemProps> = ({ tag, link }) => {
+    return (
+      <div className='menu-item'>
+        <Link onClick={toggleMenu} href={link}>
+          {tag}
+        </Link>
+      </div>
+    );
+  };
+
   return (
-    <div className='header'>
-      <div className='flex justify-between w-full mx-auto'>
-        <Link href='/'>
-          <div className='logo'>
-            <div>h-town dao</div>
-          </div>
+    <div className='header flex flex-col'>
+      <div className='flex justify-between items-center w-full mx-auto p-4 relative'>
+        <Link className='logo' href='/'>
+          <div>h-town dao</div>
         </Link>
 
-        <div className='lg:hidden'>
-          <span className='text-2xl cursor-pointer' onClick={toggleMenu} onMouseDown={(e) => e.preventDefault()}>
-            {menuOpen ? <RxCross1 /> : <RxHamburgerMenu />}
-          </span>
+        <div className='flex items-center'>
+          <div className='lg:hidden'>
+            <span className='text-2xl cursor-pointer' onClick={toggleMenu} onMouseDown={(e) => e.preventDefault()}>
+              {menuOpen ? <RxCross1 /> : <RxHamburgerMenu />}
+            </span>
+          </div>
+
+          {/* Always visible on larger screens */}
+          <div className='hidden lg:flex gap-5'>
+            <MenuItem tag='About Us' link='/about-us' />
+            <MenuItem tag='Schedule' link='/schedule' />
+            <MenuItem tag='Contact' link='/contact' />
+          </div>
         </div>
 
         {/* Conditionally render this div based on menuOpen state */}
         {menuOpen && (
-          <div className='right flex flex-col gap-5'>
+          <div className='lg:hidden flex flex-col absolute top-full left-0 w-full border-solid border-2'>
             <MenuItem tag='About Us' link='/about-us' />
             <MenuItem tag='Schedule' link='/schedule' />
             <MenuItem tag='Contact' link='/contact' />
           </div>
         )}
-
-        {/* Always visible on larger screens */}
-        <div className='menu-container'>
-          {/* Always visible on larger screens */}
-          <div className='hidden right lg:flex gap-5'>
-            <MenuItem tag='About Us' link='/about-us' />
-            <MenuItem tag='Schedule' link='/schedule' />
-            <MenuItem tag='Contact' link='/contact' />
-          </div>
-        </div>
       </div>
     </div>
   );

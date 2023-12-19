76490aef75f22c67d42201f5af55292694f9a280
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-12-19 17:58:29 -0600

    added 2 speakers

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -7,8 +7,23 @@ const Schedule = () => {
       <div className='flex text-3xl pt-10 justify-center'>2024 Houston Web3 Schedule</div>
       <div className='pt-5'>
         <div className='lg:grid grid-cols-2 gap-5'>
-          <div className=''></div>
-          <div className=''></div>
+          <div className=''>
+            <img src={info.schedule[202402].image}></img>
+          </div>
+          <div className=''>
+            <div className='text-3xl text-center'>Cartesi - Feb 12</div>
+            {info.schedule[202402].description}
+          </div>
+        </div>
+        <br />
+        <div className='lg:grid grid-cols-2 gap-5'>
+          <div className=''>
+            <img src={info.schedule[202403].image}></img>
+          </div>
+          <div className=''>
+            <div className='text-3xl text-center'>Mining - Feb 12</div>
+            {info.schedule[202403].description}
+          </div>
         </div>
       </div>
     </div>

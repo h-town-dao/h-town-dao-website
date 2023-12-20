a509745058c48ba320251db47ef947b7902b3e81
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-12-20 11:26:31 -0600

    adjusted formating for 2024 schedule

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,30 +1,41 @@
 import { info } from "@/data/data";
 import Image from "next/image";
 
+type BioSectionProps = {
+  title: string;
+  imageUrl: string;
+  description: string;
+};
+
+const BioSection: React.FC<BioSectionProps> = ({ title, imageUrl, description }) => {
+  return (
+    <div className='lg:grid grid-cols-2 gap-5 pb-5'>
+      <div className='flex justify-center items-center'>
+        <img className='w-80 h-80 pb-5' src={imageUrl} alt='Bio Image' />
+      </div>
+      <div className=''>
+        <div className='text-2xl text-center pb-5'>{title}</div>
+        <div className=''>{description}</div>
+      </div>
+    </div>
+  );
+};
+
 const Schedule = () => {
   return (
     <div className=''>
       <div className='flex text-3xl pt-10 justify-center'>2024 Houston Web3 Schedule</div>
-      <div className='pt-5'>
-        <div className='lg:grid grid-cols-2 gap-5 w-full'>
-          <div className='flex justify-center items-center'>
-            <img className='w-80 h-80' src={info.schedule[202402].image} />
-          </div>
-          <div className=''>
-            <div className='text-3xl text-center'>Cartesi - Feb 12</div>
-            {info.schedule[202402].description}
-          </div>
-        </div>
-        <br />
-        <div className='lg:grid grid-cols-2 gap-5'>
-          <div className='flex justify-center items-center'>
-            <img className='w-80 h-80' src={info.schedule[202403].image} />
-          </div>
-          <div className=''>
-            <div className='text-3xl text-center'>Mining - March 15</div>
-            {info.schedule[202403].description}
-          </div>
-        </div>
+      <div className='pt-5 pb-5'>
+        <BioSection
+          title={info.schedule[202402].title}
+          imageUrl={info.schedule[202402].image}
+          description={info.schedule[202402].description}
+        />
+        <BioSection
+          title={info.schedule[202403].title}
+          imageUrl={info.schedule[202403].image}
+          description={info.schedule[202403].description}
+        />
       </div>
     </div>
   );
--- src/data/data.ts
+++ src/data/data.ts
@@ -10,15 +10,15 @@ const info = {
   schedule: {
     2024_01: { description: "test", image: "/images/download.png" },
     2024_02: {
-      title: "Zach Prater",
+      title: "Cartesi - Zach Prater - Feb 12",
       description:
         "Zach Prater is a key member of an Ecosystem Growth Team at Cartesi, specializing in organizing hackathons, overseeing grants, and linking startups with venture capitalists. Zac's engagement in the Web3 sector began in 2015, marking an exhilarating journey. With experience at several blockchain companies, Zac harbors ambitions to establish their own venture in the future.",
       image: "/images/prater.jpg",
     },
     2024_03: {
-      title: "George Pate",
+      title: "Mining - George Pate - Feb 15",
       description:
-        "I am a highly motivated IT and cybersecurity professional with over 15 years of experience in both the private and public sectors. My expertise encompasses a wide range of areas, including cybersecurity initiatives, server hardware, OSINT, network management, software development, and more. I have a strong background in cloud technologies, such as AWS, Azure, and GCP, and I'm well-versed in IT asset management and Active Directory. Additionally, I have a diverse set of skills, including scripting in PowerShell, UNIX, C++, HTML, JavaScript, Python, and Bash. Outside of my professional life, I'm an enthusiast of various interests, such as micro green farming, ham radios, solar energy, and classic car restoration. I thrive on the synergy of technologies and constantly seek innovative solutions. My goal is to harness technology to create abundance and make a positive impact on the world. Hack the Planet!",
+        "George Pate is a highly motivated IT and cybersecurity professional with over 15 years of experience in both the private and public sectors. My expertise encompasses a wide range of areas, including cybersecurity initiatives, server hardware, OSINT, network management, software development, and more. I have a strong background in cloud technologies, such as AWS, Azure, and GCP, and I'm well-versed in IT asset management and Active Directory. Additionally, I have a diverse set of skills, including scripting in PowerShell, UNIX, C++, HTML, JavaScript, Python, and Bash. Outside of my professional life, I'm an enthusiast of various interests, such as micro green farming, ham radios, solar energy, and classic car restoration. I thrive on the synergy of technologies and constantly seek innovative solutions. My goal is to harness technology to create abundance and make a positive impact on the world. Hack the Planet!",
       image: "/images/pate.jpg",
     },
     2024_04: { description: "test", image: "/images/download.png" },

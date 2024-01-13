c53d4de88cc82d8a26a044b1b82ef97fe929f704
giorgio-villani <giorgio-villani@users.noreply.github.com>
2024-01-13 14:53:13 -0600

    updated date for cartesi talk

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,29 +1,29 @@
 const info = {
   links: {
-    instagram_link: "https://www.instagram.com/htxweb3/",
-    twitter_link: "https://www.instagram.com/htxweb3/",
-    ethereum_meetup_link: "https://www.meetup.com/houstonethereum/",
-    web3_meetup_link: "https://www.meetup.com/htownweb3/",
-    github_link: "https://github.com/h-town-dao",
+    instagram_link: 'https://www.instagram.com/htxweb3/',
+    twitter_link: 'https://www.instagram.com/htxweb3/',
+    ethereum_meetup_link: 'https://www.meetup.com/houstonethereum/',
+    web3_meetup_link: 'https://www.meetup.com/htownweb3/',
+    github_link: 'https://github.com/h-town-dao',
   },
   mission_statement: "To unlock Houston's Web3 community potential",
   schedule: {
-    2024_01: { description: "test", image: "/images/download.png" },
+    2024_01: { description: 'test', image: '/images/download.png' },
     2024_02: {
-      title: "Cartesi - Zach Prater - Feb 12",
+      title: 'Cartesi - Zach Prater - Feb 19',
       description:
         "Zach Prater is a key member of an Ecosystem Growth Team at Cartesi, specializing in organizing hackathons, overseeing grants, and linking startups with venture capitalists. Zac's engagement in the Web3 sector began in 2015, marking an exhilarating journey. With experience at several blockchain companies, Zac harbors ambitions to establish their own venture in the future.",
-      image: "/images/prater.jpg",
+      image: '/images/prater.jpg',
     },
     2024_03: {
-      title: "Mining - George Pate - Mar 15",
+      title: 'Mining - George Pate - Mar 15',
       description:
         "George Pate is a highly motivated IT and cybersecurity professional with over 15 years of experience in both the private and public sectors. My expertise encompasses a wide range of areas, including cybersecurity initiatives, server hardware, OSINT, network management, software development, and more. I have a strong background in cloud technologies, such as AWS, Azure, and GCP, and I'm well-versed in IT asset management and Active Directory. Additionally, I have a diverse set of skills, including scripting in PowerShell, UNIX, C++, HTML, JavaScript, Python, and Bash. Outside of my professional life, I'm an enthusiast of various interests, such as micro green farming, ham radios, solar energy, and classic car restoration. I thrive on the synergy of technologies and constantly seek innovative solutions. My goal is to harness technology to create abundance and make a positive impact on the world. Hack the Planet!",
-      image: "/images/pate.jpg",
+      image: '/images/pate.jpg',
     },
-    2024_04: { description: "test", image: "/images/download.png" },
-    2024_05: { description: "test", image: "/images/download.png" },
-    2024_06: { description: "test", image: "/images/download.png" },
+    2024_04: { description: 'test', image: '/images/download.png' },
+    2024_05: { description: 'test', image: '/images/download.png' },
+    2024_06: { description: 'test', image: '/images/download.png' },
   },
 };
 

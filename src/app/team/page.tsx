aaf5362408d90b33ee74e295107615132a76e6d5
giorgio-villani <giorgio-villani@users.noreply.github.com>
2024-01-25 18:34:53 -0600

    added website link

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -5,9 +5,10 @@ type BioSectionProps = {
   title: string;
   imageUrl: string;
   description: string;
+  website: string;
 };
 
-const BioSection: React.FC<BioSectionProps> = ({ title, imageUrl, description }) => {
+const BioSection: React.FC<BioSectionProps> = ({ title, imageUrl, description, website }) => {
   return (
     <div className="lg:grid grid-cols-2 gap-5 pb-10">
       <div className="flex justify-center items-center">
@@ -16,6 +17,14 @@ const BioSection: React.FC<BioSectionProps> = ({ title, imageUrl, description })
       <div className="pt">
         <div className="text-2xl text-center pb-5">{title}</div>
         <div className="">{description}</div>
+        {website && (
+          <div className="pt-5">
+            <span>Website: </span>
+            <a href={website} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
+              {website}
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -26,7 +35,7 @@ const Schedule = () => {
     <div className="">
       <div className="flex text-3xl pt-10 justify-center">H-Town DAO Team</div>
       <div className="pt-5 pb-5">
-        <BioSection title={info.team.giorgio.name} imageUrl={info.team.giorgio.image} description={info.team.giorgio.bio} />
+        <BioSection title={info.team.giorgio.name} imageUrl={info.team.giorgio.image} description={info.team.giorgio.bio} website={info.team.giorgio.website} />
         <BioSection title={info.team.margaret.name} imageUrl={info.team.margaret.image} description={info.team.margaret.bio} />
       </div>
     </div>

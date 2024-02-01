3d6e7cd8feb9ad40efb125a8344211f3a70a0428
giorgio-villani <giorgio-villani@users.noreply.github.com>
2024-02-01 15:24:46 -0600

    adjusted text for speaker form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
             </li>
             <li>Contribute to our open-source project on {create_link('Github', info.links.github_link)}</li>
             <li>Fill out the H-Town DAO interest {create_link('Form', info.links.google_interest_form)}</li>
-            {/* <li>Fill out speaker {create_link('Form', info.links.google_speaker_form)}</li> */}
+            <li>Fill out Web3 speaker {create_link('Form', info.links.google_speaker_form)}</li>
           </ul>
         </div>
       </div>
